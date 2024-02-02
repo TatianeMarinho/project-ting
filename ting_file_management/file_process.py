2649c2390ca5628171f96a62397255b7088f89a4
TatianeMarinho <devtatianemarinho@gmail.com>
2024-02-01 20:52:48 -0400

    função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in instance._data:
+        if path_file == item['nome_do_arquivo']:
+            return
+
+    lines = txt_importer(path_file)
+
+    file_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(file_data)
+
+    sys.stdout.write(f"{file_data}")
 
 
 def remove(instance):
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,23 +3,23 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self.__data = []
+        self._data = []
 
     def __len__(self):
-        return len(self.__data)
+        return len(self._data)
 
     def is_empty(self):
         return not self.__len__()
 
     def enqueue(self, value):
-        self.__data.append(value)
+        self._data.append(value)
 
     def dequeue(self):
         if self.is_empty():
             raise IndexError('Vazio')
-        return self.__data.pop(0)
+        return self._data.pop(0)
 
     def search(self, index):
         if index < 0 or index >= len(self):
             raise IndexError("Índice Inválido ou Inexistente")
-        return self.__data[index]
+        return self._data[index]
