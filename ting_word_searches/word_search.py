17b1896e28cbf765191726aa38e1c641d2c17512
TatianeMarinho <devtatianemarinho@gmail.com>
2024-02-15 17:10:30 -0400

    função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []  # armazena as ocorrencias das palavras
+
+    for item in instance._data:  # itero sobre cada item da fila
+        file_name = item["nome_do_arquivo"]  # extraio o nome do arquivo
+        lines = item["linhas_do_arquivo"]  # linhas do arq p/buscar a pal.
+        file_occurrences = []  # armaz.ocorrencias no arq. atual
+
+        for index, line in enumerate(lines, start=1):  # itera l do arq atual
+            if word.lower() in line.lower():  # conv. e verif. a palavra
+                file_occurrences.append({"linha": index})
+                # armaz. a linha onde a palavra foi encontrada
+
+        if file_occurrences:  # se tem ocorrencias
+            occurrences.append({  # armaz. os dados 
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": file_occurrences
+            })
+
+    return occurrences
 
 
 def search_by_word(word, instance):
